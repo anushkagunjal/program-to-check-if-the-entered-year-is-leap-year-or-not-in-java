8e85412cdf1bd992d2ecf7a397a9dde1e8b55a4d
anushkagunjal <anushkagunjal2017@gmail.com>
2022-01-03 12:31:15 +0530

    first commit

---
+++ Leap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Leap{
+    public static void main(String[] args) {
+        System.out.print("Enter a Year:");
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        
+        if(year%4==0 && year%100!=0 || year%100==0 && year%400==0){
+            System.out.println(year+" "+"is Leap year");
+        }
+        else{
+            System.out.println(year+" "+"is Not a Leap year");
+        }
+        
+    }
+}
